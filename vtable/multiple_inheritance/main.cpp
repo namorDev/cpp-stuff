a06d2b457fcf94318b84b40b92448fdf6d83d280
Roman Bärtschi <50102152+namorDev@users.noreply.github.com>
2024-02-27 13:11:03 +0100

    Update main.cpp

--- vtable/multiple_inheritance/main.cpp
+++ vtable/multiple_inheritance/main.cpp
@@ -6,21 +6,35 @@
 class A
 {
 public:
-    int foo(int a){std::cout << "A" << std::endl; return 0;}
+    virtual void foo(int a){std::cout << "A" << std::endl; return 0;}
 };
 
 class B
 {
 public:
-  bool foo(int a){ std::cout << "B" << std::endl; return true;}
+  virtual void foo(int a){ std::cout << "B" << std::endl; return true;}
 };
 
+class AdapterA : A
+{
+public:
+void fooA(int a) { foo(); }
+}
 
-class Child : A,B
+class AdapterB : B
+{
+public:
+void fooB(int a) { foo(); }
+}
+
+class Child : AdapterA, AdapterB
 {
 
   public:
 //    bool foo(int a){ std::cout << "Child" << std::endl; return true;}
+// The methods of A and B have the same signature but I want to override or use each individually
+// With a minimized vtable
+
 };
 
 int main()
